b72e3af890d1f1ff3ed3ca4bd5b225296a5a705f
N_F <N_F@DESKTOP-6RD84GH>
2018-06-05 15:02:22 +0900

    中村：DBUtils、JavaBeansの作成

---
+++ src/abs2/utils/DBUtils.java
@@ -0,0 +1,28 @@
+package abs2.utils;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+
+public class DBUtils {
+	public static Connection getConnection() throws NamingException, SQLException {
+
+		Context initContext = new InitialContext();
+		Context envContext = (Context) initContext.lookup("java:/comp/env");
+		DataSource ds = (DataSource) envContext.lookup("abs2");
+
+		return ds.getConnection();
+
+	}
+
+	public static void close(Connection con) throws SQLException {
+		if (con != null) {
+			con.close();
+		}
+	}
+}
