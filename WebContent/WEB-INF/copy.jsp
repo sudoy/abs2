b713e2e258c8504a00407d31ad6b49ca52c5fed0
kanauchi <kanauchi@tsd-201601-0009>
2018-06-06 15:52:55 +0900

    copy

--- WebContent/WEB-INF/copy.jsp
+++ WebContent/WEB-INF/copy.jsp
@@ -1,5 +1,6 @@
 <%@page contentType="text/html; charset=UTF-8"%>
 <%@page pageEncoding="UTF-8"%>
+<%@ page import="abs2.utils.HTMLUtils"%>
 
 <jsp:include page="_header.jsp" />
 
@@ -32,11 +33,11 @@
 
 		<hr class="mt-1">
 
-		<form action="#" method="post">
+v		<form action="copy.html?id=${param.id != null ? param.id : myhab.id}" method="post">
 			<div class="form-group row">
 				<label for="date" class="offset-2 col-sm-2 col-form-label font-weight-bold">日付 <span class="badge badge-danger">必須</span></label>
 				<div class="col-2">
-					<input type="text" class="form-control" id="date" placeholder="日付" aria-describedby="dateHelp" value="2018/05/31">
+					<input type="text" class="form-control" id="date" name="dating" placeholder="日付" aria-describedby="dateHelp" value="${param.dating != null ? param.dating : myhab.dating}">
 				</div>
 				<div class="col-4">
 					<small id="dateHelp" class="text-muted align-bottom">「YYYY/MM/DD」形式で入力してください。</small>
@@ -48,11 +49,13 @@
 					<legend class="offset-2 col-form-label col-2 pt-0 font-weight-bold">区分</legend>
 					<div class="col-sm-8">
 						<div class="custom-control custom-radio custom-control-inline">
-							<input type="radio" id="division1" name="division" class="custom-control-input" checked>
+							<input type="radio" id="division1" name="in_out" class="custom-control-input" 
+							value="1" ${HTMLUtils.checkInOut(param.in_out != null? param.in_out : myhab.inOut, '1')}>
 							<label class="custom-control-label" for="division1">支出</label>
 						</div>
 						<div class="custom-control custom-radio custom-control-inline">
-							<input type="radio" id="division2" name="division" class="custom-control-input">
+							<input type="radio" id="division2" name="in_out" class="custom-control-input" 
+							value="2" ${HTMLUtils.checkInOut(param.in_out != null? param.in_out : myhab.inOut, '2')}>
 							<label class="custom-control-label" for="division2">収入</label>
 						</div>
 					</div>
@@ -62,31 +65,31 @@
 			<div class="form-group row">
 				<label for="category" class="offset-2 col-sm-2 col-form-label font-weight-bold">カテゴリー <span class="badge badge-danger">必須</span></label>
 				<div class="col-4">
-					<select class="custom-select" id="category">
+					<select class="custom-select" name="category" id="category">
 						<option>選択して下さい</option>
-						<option selected>食費</option>
-						<option>日用品</option>
-						<option>交際費</option>
+						<option value="1" ${HTMLUtils.checkCategory(param.category != null? param.category : myhab.category, '1')}>食費</option>
+						<option value="2" ${HTMLUtils.checkCategory(param.category != null? param.category : myhab.category, '2')}>日用品</option>
+						<option value="3" ${HTMLUtils.checkCategory(param.category != null? param.category : myhab.category, '3')}>交際費</option>
 					</select>
 				</div>
 			</div>
 			<div class="form-group row">
 				<label for="note" class="offset-2 col-sm-2 col-form-label font-weight-bold">備考</label>
 				<div class="col-6">
-					<textarea class="form-control" id="note" placeholder="備考" rows="3">ランチ</textarea>
+					<textarea class="form-control" id="note" name="memo" placeholder="備考" rows="3">${param.memo != null ? param.memo : myhab.memo}</textarea>
 				</div>
 			</div>
 			<div class="form-group row">
 				<label for="amount" class="offset-2 col-sm-2 col-form-label font-weight-bold">金額 <span class="badge badge-danger">必須</span></label>
 				<div class="col-2">
-					<input type="text" class="form-control" id="amount" placeholder="金額" value="800">
+					<input type="text" class="form-control" id="amount" name="money"placeholder="金額" value="${param.money != null ? param.money : myhab.money}">
 				</div>
 			</div>
 
 			<div class="form-group row">
 				<div class="offset-4 col-8">
 					<a href="index.html" class="btn btn-secondary">キャンセル</a>
-					<a href="index.html" class="btn btn-primary"><span class="oi oi-check"></span> コピーOK</a>
+					<input type="submit" class="btn btn-primary" value="コピーOK">
 				</div>
 			</div>
 		</form>
--- src/abs2/CopyServlet.java
+++ src/abs2/CopyServlet.java
@@ -4,7 +4,9 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -17,63 +19,141 @@ import abs2.utils.DBUtils;
 
 @WebServlet("/copy.html")
 public class CopyServlet extends HttpServlet {
-		@Override
-		protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-			req.setCharacterEncoding("utf-8");
-			Connection con = null;
-			PreparedStatement ps = null;
-			String sql = null;
-			ResultSet rs = null;
+		req.setCharacterEncoding("utf-8");
+		Connection con = null;
+		PreparedStatement ps = null;
+		String sql = null;
+		ResultSet rs = null;
 
+		try {
+			con = DBUtils.getConnection();
+
+			sql = "select * from myhab where id = ?";
+			//select命令の準備
+			ps = con.prepareStatement(sql);
+
+			//select文にパラメータの内容をセット
+			ps.setString(1, req.getParameter("id"));
+
+			//select命令を実行
+			rs = ps.executeQuery();
+
+			rs.next();
+
+			int id = rs.getInt("id");
+			Date dating = rs.getDate("dating");
+			int inout = rs.getInt("in_out");
+			String category = rs.getString("category");
+			String memo = rs.getString("memo");
+			int money = rs.getInt("money");
+
+			Myhab myhab = new Myhab(id, dating, inout, category, memo, money);
+			req.setAttribute("myhab", myhab);
+
+			//JSPへフォワード
+			getServletContext().getRequestDispatcher("/WEB-INF/copy.jsp")
+					.forward(req, resp);
+
+		} catch (Exception e) {
+			throw new ServletException(e);
+		} finally {
 			try {
-				con = DBUtils.getConnection();
-
-				sql = "select * from myhab where id = ?";
-				//select命令の準備
-				ps = con.prepareStatement(sql);
-
-				//select文にパラメータの内容をセット
-				ps.setString(1, req.getParameter("id"));
-
-				//select命令を実行
-				rs = ps.executeQuery();
-
-				rs.next();
-
-				int id = rs.getInt("id");
-				Date dating = rs.getDate("dating");
-				int inout = rs.getInt("in_out");
-				String category = rs.getString("category");
-				String memo = rs.getString("memo");
-				int money = rs.getInt("money");
-
-				Myhab myhab = new Myhab(id, dating, inout, category, memo, money);
-				req.setAttribute("myhab", myhab);
-
-				//JSPへフォワード
-				getServletContext().getRequestDispatcher("/WEB-INF/copy.jsp")
-						.forward(req, resp);
-
-			} catch (Exception e) {
-				throw new ServletException(e);
-			} finally {
-				try {
-					if (rs != null) {
-						con.close();
-					}
-					if (ps != null) {
-						ps.close();
-					}
-					DBUtils.close(con);
-				} catch (Exception e) {
+				if (rs != null) {
+					con.close();
 				}
+				if (ps != null) {
+					ps.close();
+				}
+				DBUtils.close(con);
+			} catch (Exception e) {
 			}
 		}
+	}
 
-		@Override
-		protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		req.setCharacterEncoding("utf-8");
+		//HttpSession session = req.getSession();
 
-			getServletContext().getRequestDispatcher("/WEB-INF/copy.jsp").forward(req, resp);
+		String dating = req.getParameter("dating");
+		String inout = req.getParameter("in_out");
+		String category = req.getParameter("category");
+		String memo = req.getParameter("memo");
+		String money = req.getParameter("money");
+
+		//バリデーションチェック
+		List<String> errors = validate(dating, inout, category, memo, money);
+		if (errors.size() > 0) {
+			req.setAttribute("errors", errors);
+			getServletContext().getRequestDispatcher("/WEB-INF/entry.jsp").forward(req, resp);
+			return;
+		}
+
+		Connection con = null;
+		PreparedStatement ps = null;
+		String sql = null;
+
+		try {
+			con = DBUtils.getConnection();
+
+			sql = "insert into myhab(dating, in_out, category, memo, money) value(?, ?, ?, ?, ?);";
+			ps = con.prepareStatement(sql);
+
+			ps.setString(1, dating);
+			ps.setString(2, inout);
+			ps.setString(3, category);
+			ps.setString(4, memo);
+			if (inout.equals("2")) {
+				ps.setString(5, money);
+			} else {
+				ps.setString(5, "-" + money);
+			}
+
+			ps.executeUpdate();
+			List<String> successes = new ArrayList<>();
+			successes.add("登録しました。");
+			//session.setAttribute("successes", successes);
+			req.setAttribute("successes", successes);
+
+			resp.sendRedirect("index.html");
+
+		} catch (Exception e) {
+			throw new ServletException(e);
+		} finally {
+			try {
+				if (con != null) {
+					con.close();
+				}
+				if (ps != null) {
+					ps.close();
+				}
+			} catch (Exception e) {
+			}
 		}
 	}
+
+	private List<String> validate(String dating, String inout, String category, String memo, String money) {
+		List<String> errors = new ArrayList<>();
+
+		//日付の必須入力
+		if (dating.equals("")) {
+			errors.add("日付は必須入力です。");
+		}
+
+		//カテゴリーの必須入力
+		if (!category.equals("1") && !category.equals("2")) {
+			errors.add("カテゴリーは必須入力です。");
+		}
+
+		//金額の必須入力
+		if (money.equals("")) {
+			errors.add("金額は必須入力です。");
+		}
+
+		return errors;
+	}
+}
